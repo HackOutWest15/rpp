f9ba51a07cb59e03a9246b14140d967c0caea7d7
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2015-08-11 13:48:42 +0200

    Minor fixes, but wont work until we have a working callback uri

--- Places/app/src/main/java/com/redpandateam/places/MainActivity.java
+++ Places/app/src/main/java/com/redpandateam/places/MainActivity.java
@@ -1,32 +1,28 @@
 package com.redpandateam.places;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.content.Intent;
-import android.net.Uri;
-import android.util.Log;
-import android.view.View;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 
+import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
 import com.spotify.sdk.android.player.Config;
-import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.player.ConnectionStateCallback;
 import com.spotify.sdk.android.player.Player;
 import com.spotify.sdk.android.player.PlayerNotificationCallback;
 import com.spotify.sdk.android.player.PlayerState;
 
-public class MainActivity extends AppCompatActivity implements PlayerNotificationCallback, ConnectionStateCallback {
+public class MainActivity extends Activity implements
+        PlayerNotificationCallback, ConnectionStateCallback {
+
     // TODO: Replace with your client ID
     private static final String CLIENT_ID = "5ca32746c9214541a8824cf942ce7dd9";
     // TODO: Replace with your redirect URI
-    private static final String REDIRECT_URI = "placeslogin://callback";
+    private static final String REDIRECT_URI = "localhost:9999/songplaces";
 
-    // Request code that will be used to verify if the result comes from correct activity
+    // Request code that will be passed together with authentication result to the onAuthenticationResult callback
     // Can be any integer
     private static final int REQUEST_CODE = 1337;
 
@@ -37,9 +33,8 @@ public class MainActivity extends AppCompatActivity implements PlayerNotificatio
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
-                AuthenticationResponse.Type.TOKEN,
-                REDIRECT_URI);
+        AuthenticationRequest.Builder builder =
+                new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
         builder.setScopes(new String[]{"user-read-private", "streaming"});
         AuthenticationRequest request = builder.build();
 
@@ -47,54 +42,31 @@ public class MainActivity extends AppCompatActivity implements PlayerNotificatio
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+                mPlayer = Spotify.getPlayer(playerConfig, this, new Player.InitializationObserver() {
+                    @Override
+                    public void onInitialized(Player player) {
+                        mPlayer.addConnectionStateCallback(MainActivity.this);
+                        mPlayer.addPlayerNotificationCallback(MainActivity.this);
+                        mPlayer.play("spotify:track:2TpxZ7JUBn3uw46aR7qd6V");
+                    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+                    }
+                });
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
-
-   @Override
-   protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-       super.onActivityResult(requestCode, resultCode, intent);
-
-       // Check if result comes from the correct activity
-       if (requestCode == REQUEST_CODE) {
-           AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
-           if (response.getType() == AuthenticationResponse.Type.TOKEN) {
-               Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
-               mPlayer = Spotify.getPlayer(playerConfig, this, new Player.InitializationObserver() {
-                   @Override
-                   public void onInitialized(Player player) {
-                       mPlayer.addConnectionStateCallback(MainActivity.this);
-                       mPlayer.addPlayerNotificationCallback(MainActivity.this);
-                       //TODO: remove this for dynamic choice of song
-                       mPlayer.play("spotify:track:2TpxZ7JUBn3uw46aR7qd6V");
-                   }
-
-                   @Override
-                   public void onError(Throwable throwable) {
-                       Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
-                   }
-               });
-           }
-       }
-   }
     @Override
     public void onLoggedIn() {
         Log.d("MainActivity", "User logged in");
@@ -123,15 +95,26 @@ public class MainActivity extends AppCompatActivity implements PlayerNotificatio
     @Override
     public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
         Log.d("MainActivity", "Playback event received: " + eventType.name());
+        switch (eventType) {
+            // Handle event type as necessary
+            default:
+                break;
+        }
     }
 
     @Override
     public void onPlaybackError(ErrorType errorType, String errorDetails) {
         Log.d("MainActivity", "Playback error received: " + errorType.name());
+        switch (errorType) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
     }
 
     @Override
     protected void onDestroy() {
+        // VERY IMPORTANT! This must always be called or else you will leak resources
         Spotify.destroyPlayer(this);
         super.onDestroy();
     }
